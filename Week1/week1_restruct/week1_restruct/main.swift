b6e711321f4cfd89ef38051600c75bf6cae44926
OneE <top9712@daum.net>
2022-05-23 21:02:57 +0900

    feat: 아이스크림만들기 함수 초안

--- Week1/week1_restruct/week1_restruct/main.swift
+++ Week1/week1_restruct/week1_restruct/main.swift
@@ -1,4 +1,35 @@
 import Foundation
 
-print("Hello, World!")
+let bodyComponent: String = "*"
+let spaceForBarLine: String = " "
+let spaceForBarLineLength: Int = 4
+let bodyLineLength: Int = 11
 
+func drawIceCreamBody(bodyLength: Int) {
+    for _ in 1...bodyLength {
+        drawIceCreamBodyLine()
+    }
+}
+
+func drawIceCreamBodyLine() {
+    for _ in 1...bodyLineLength {
+        print(bodyComponent, terminator: "")
+    }
+    print("")
+}
+
+func drawIceCreamBar(barLength: Int) {
+    for _ in 1...barLength {
+        for _ in 1...spaceForBarLineLength {
+            print(spaceForBarLine,terminator: "")
+        }
+        print("| |")
+    }
+}
+
+func drawIceCream(bodyLength: Int, barLength: Int) {
+    drawIceCreamBody(bodyLength: bodyLength)
+    drawIceCreamBar(barLength: barLength)
+}
+
+drawIceCream(bodyLength: 8, barLength: 4)
